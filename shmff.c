1e4d6b25f33ea2b5c54bf207714b044c00ccc9fe
Jan Klemkow <j.klemkow@wemelug.de>
2016-03-16 23:45:21 +0100

    wip

--- shmff.c
+++ shmff.c
@@ -14,9 +14,42 @@
 #define KEY_ID_WRITE 1	/* write shm id */
 
 void
+shm2file(const char *file, struct hdr *hdr, struct px *ff)
+{
+	FILE *fh = stdout;
+	size_t px_n = hdr->width * hdr->height;
+
+	if (file != NULL) {
+		if ((fh = fopen(file, "w")) == NULL)
+			err(EXIT_FAILURE, "fopen");
+	}
+
+	hdr->width = htonl(hdr->width);
+	hdr->height = htonl(hdr->height);
+
+	/* save header */
+	if (fwrite(hdr, sizeof *hdr, 1, fh) < 1)
+		err(EXIT_FAILURE, "fread");
+
+	for (size_t p = 0; p < px_n; p++) {
+		/* convert net endian to host endian */
+		ff[p].red   = htons(ff[p].red);
+		ff[p].green = htons(ff[p].green);
+		ff[p].blue  = htons(ff[p].blue);
+		ff[p].alpha = htons(ff[p].alpha);
+
+		if (fwrite(&ff[p], sizeof *ff, 1, fh) < 1)
+			err(EXIT_FAILURE, "unexpected end-of-file");
+	}
+
+	if (fclose(fh) == EOF)
+		err(EXIT_FAILURE, "fclose");
+}
+
+void
 usage(void)
 {
-	fputs("send <file>\n", stderr);
+	fputs("shmff <in file> <out file>\n", stderr);
 	exit(EXIT_FAILURE);
 }
 
@@ -26,19 +59,21 @@ main(int argc, char *argv[])
 	int shmid;
 	key_t key_r;	/* read page */
 	key_t key_w;	/* write page */
-	FILE *fh;
+	FILE *fh = stdin;
 	FILE *ph;
+	char *file = NULL;
 
 	struct hdr hdr;
-	struct px *ff = NULL;
+	struct px *ff_r = NULL;
+	struct px *ff_w = NULL;
 
-	if (argc < 2)
-		usage();
+	if (argc >= 2)
+		file = argv[1];
 
-	char *file = argv[1];
 
-	if ((fh = fopen(file, "r")) == NULL)
-		err(EXIT_FAILURE, "fopen");
+	if (file != NULL)
+		if ((fh = fopen(file, "r")) == NULL)
+			err(EXIT_FAILURE, "fopen");
 
 	/* read header */
 	if (fread(&hdr, sizeof hdr, 1, fh) < 1)
@@ -51,9 +86,9 @@ main(int argc, char *argv[])
 	hdr.width = ntohl(hdr.width);
 	hdr.height = ntohl(hdr.height);
 
-	size_t px_n = hdr.width * hdr.width;
-	size_t ff_size = hdr.width * hdr.height * sizeof(*ff);
-	printf("ff mem size: %zu\n", ff_size);
+	size_t px_n = hdr.width * hdr.height;
+	size_t ff_size = hdr.width * hdr.height * sizeof(struct px);
+	fprintf(stderr, "ff mem size: %zu\n", ff_size);
 
 	/* generate name of shm segment by file path */
 	key_r = ftok(file, KEY_ID_READ);
@@ -62,29 +97,34 @@ main(int argc, char *argv[])
 	/* Create and attach the shm segment for reading.  */
 	if ((shmid = shmget(key_r, ff_size, IPC_CREAT | 0666)) < 0)
 		err(EXIT_FAILURE, "shmget");
-	if ((ff = shmat(shmid, NULL, 0)) == (void *) -1)
+	if ((ff_r = shmat(shmid, NULL, 0)) == (void *) -1)
 		err(EXIT_FAILURE, "shmat");
 
 	/* Create and attach the shm segment for writing.  */
 	if ((shmid = shmget(key_w, ff_size, IPC_CREAT | 0666)) < 0)
 		err(EXIT_FAILURE, "shmget");
-	if ((ff = shmat(shmid, NULL, 0)) == (void *) -1)
+	if ((ff_w = shmat(shmid, NULL, 0)) == (void *) -1)
 		err(EXIT_FAILURE, "shmat");
 
 	/* read image into memory */
 	for (size_t p = 0; p < px_n; p++) {
-		if (fread(&ff[p], sizeof *ff, 1, fh) < 1)
+		if (fread(&ff_r[p], sizeof *ff_r, 1, fh) < 1)
 			err(EXIT_FAILURE, "unexpected end-of-file");
 
 		/* convert net endian to host endian */
-		ff[p].red   = ntohs(ff[p].red);
-		ff[p].green = ntohs(ff[p].green);
-		ff[p].blue  = ntohs(ff[p].blue);
-		ff[p].alpha = ntohs(ff[p].alpha);
+		ff_r[p].red   = ntohs(ff_r[p].red);
+		ff_r[p].green = ntohs(ff_r[p].green);
+		ff_r[p].blue  = ntohs(ff_r[p].blue);
+		ff_r[p].alpha = ntohs(ff_r[p].alpha);
 	}
 
-	/* start program */
-	if ((ph = popen("./blur", "w")) == NULL)
+	if (fclose(fh) == EOF)
+		err(EXIT_FAILURE, "fclose");
+
+	/*
+	 * start converting program
+	 */
+	if ((ph = popen("./invert", "w")) == NULL)
 		err(EXIT_FAILURE, "popen");
 
 	struct shm_ff shmff_r;
@@ -101,6 +141,16 @@ main(int argc, char *argv[])
 
 	if (pclose(ph) != EXIT_SUCCESS)
 		err(EXIT_FAILURE, "pclose");
+
+	/*
+	 * save image
+	 */
+	if (argc >= 3)
+		file = argv[2];
+	else
+		file = NULL;
+
+	shm2file(file, &hdr, ff_w);
 
 	exit(0);
 }
