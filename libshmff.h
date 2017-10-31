d7a99f26f2a6303412d0b1d20536d422c01f674e
Jan Klemkow <j.klemkow@wemelug.de>
2017-10-31 14:31:26 +0100

    wip

--- libshmff.h
+++ libshmff.h
@@ -3,7 +3,7 @@
 
 #include "ff.h"
 
-void setshmff(struct hdr **hdr, struct px **ff);
+void setshmff(struct shmff *shmff, struct hdr **hdr, struct px **ff);
 int fork_jobs(int jobs, size_t *off, size_t *px_n);
 int catch_jobs(int jobs, int child);
 
